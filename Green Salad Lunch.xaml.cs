afc84251dc9f2d683528f37941cd78c1e73e5ed8
william-ferguson <willyferguson@hotmail.co.nz>
2016-06-23 14:57:37 +1200

    Add files via upload

---
+++ Green Salad Lunch.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace LunchToGo
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Green_Salad_Lunch : Page
+    {
+        public Green_Salad_Lunch()
+        {
+            this.InitializeComponent();
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(page_2));
+        }
+
+        private void btnFinish_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage));
+        }
+    }
+}
---
+++ Select Meal.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace LunchToGo
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class page_2 : Page
+    {
+        public page_2()
+        {
+            this.InitializeComponent();
+        }
+
+        private void tbxGreenSaladLunch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Green_Salad_Lunch));
+        }
+
+        private void tbxLambKorma_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Lamb_Korma));
+        }
+
+        private void tbxOpenChickenSalad_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Open_Chicken_Sandwitch));
+        }
+
+        private void tbxBeefNoodleSoup_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Beef_Noodle_Salad));
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage));
+        }
+    }
+}
